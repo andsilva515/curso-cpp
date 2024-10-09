ac2ec1d036c09dff5d0579e3085c1474f57c9dc1
andsilva515 <anderson.ssilva@outlook.com>
2024-10-09 06:50:31 -0300

    segundo programa entrada

---
+++ Ex02/segundo_entrada.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int idade;
+    cout << "Qual a sua idade?" << endl;
+    cin >> idade;
+    cout << "Idade: " << idade << endl;
+
+    return 0;
+}
